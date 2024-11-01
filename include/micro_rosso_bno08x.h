1106b1cbb2fcf3664d78e3759696289ae6d4f673
xopxe <xxopxe@gmail.com>
2024-11-01 11:40:07 -0300

    cleanup

--- include/micro_rosso_bno08x.h
+++ include/micro_rosso_bno08x.h
@@ -3,15 +3,15 @@
 
 #include <Wire.h>
 
-class ImuBNO08x {
+class ImuBNO08x
+{
 public:
   ImuBNO08x();
-  static bool setup( TwoWire &wire = Wire,
-                     const char* topic_raw = "/imu/raw",
-                     const char* topic_mag = "/mag",
-                     timer_descriptor &timer_control = micro_rosso::timer_control,
-                     timer_descriptor &timer_report = micro_rosso::timer_report
-  );
+  static bool setup(TwoWire &wire = Wire,
+                    const char *topic_raw = "/imu/raw",
+                    const char *topic_mag = "/mag",
+                    timer_descriptor &timer_control = micro_rosso::timer_control,
+                    timer_descriptor &timer_report = micro_rosso::timer_report);
 };
 
-#endif  // __imu_bno08x_h
+#endif // __imu_bno08x_h
